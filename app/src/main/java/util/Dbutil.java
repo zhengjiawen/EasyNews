0769ac87fcaff9b40a52e2e95c74a2747c6162b6
zhengjiawen <756848513@qq.com>
2016-09-25 10:43:34 +0800

    bug

--- app/src/main/java/util/Dbutil.java
+++ app/src/main/java/util/Dbutil.java
@@ -1,15 +1,24 @@
 package util;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.renderscript.Sampler;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.outi.easynews.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import db.DatabaseHelper;
 import db.Values;
@@ -18,27 +27,65 @@ import util.ToastUtil;
 /**
  * Created by outi on 2016/9/24.
  */
-public class Dbutil extends AppCompatActivity {
+public class Dbutil{
 
     private DatabaseHelper dbhelper;
     private SQLiteDatabase db;
+    private List<Map<String,Object>> listNews=new ArrayList<Map<String, Object>>();
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        dbhelper=new DatabaseHelper(this,"EasyNews",1);
+    public Dbutil(Context context) {
+        dbhelper=new DatabaseHelper(context,"EasyNews",1);
         db=dbhelper.getWritableDatabase();
         init();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void DbDestroy() {
         if(dbhelper!=null){
             dbhelper.close();
         }
     }
+
+    public List<Map<String,Object>> Query(int targetType){
+        Cursor cursor=db.query("EasyNews",null,null,null,null,null,null);
+        Map<String,Object> listNew= new HashMap<String,Object>();
+        if(cursor.moveToFirst()){
+            do{
+                int type=cursor.getInt(cursor.getColumnIndex("type"));
+                if(type==targetType){
+                    String title=cursor.getString(cursor.getColumnIndex("title"));
+                    String time=cursor.getString(cursor.getColumnIndex("time"));
+                    String content=cursor.getString(cursor.getColumnIndex("content"));
+                    listNew.put("title",title);
+                    listNew.put("time",time);
+                    listNew.put("content",content);
+                    listNews.add(listNew);
+                }
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return listNews;
+    }
+
+    //重载一个不传入参数时全部查询的函数
+    public List<Map<String,Object>> Query(){
+        Cursor cursor=db.query("EasyNews",null,null,null,null,null,null);
+        Map<String,Object> listNew= new HashMap<String,Object>();
+        if(cursor.moveToFirst()){
+            do{
+                int type=cursor.getInt(cursor.getColumnIndex("type"));
+                    String title=cursor.getString(cursor.getColumnIndex("title"));
+                    String time=cursor.getString(cursor.getColumnIndex("time"));
+                    String content=cursor.getString(cursor.getColumnIndex("content"));
+                    listNew.put("title",title);
+                    listNew.put("time",time);
+                    listNew.put("content",content);
+                    listNews.add(listNew);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return listNews;
+    }
+
 
     private void init(){
         db.insert(dbhelper.getDatabaseName(),null,Values.setAndgetValues(1,"社会新闻","9月24日","电子科大获捐10.3亿"));
@@ -49,4 +96,6 @@ public class Dbutil extends AppCompatActivity {
         db.insert(dbhelper.getDatabaseName(),null,Values.setAndgetValues(3,"财经新闻","9月24日","全国房价崩盘"));
         db.insert(dbhelper.getDatabaseName(),null,Values.setAndgetValues(4,"军事新闻","9月27日","我国成功收复台湾"));
     }
+
+
 }
